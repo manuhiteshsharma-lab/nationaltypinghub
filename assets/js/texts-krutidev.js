fac7fa2188ac5563ba52bd3eb4aaa141cec721ec
manuhiteshsharma-lab <manuhiteshsharma@gmail.com>
2026-05-24 15:54:26 +0530

    Update texts-krutidev.js

--- assets/js/texts-krutidev.js
+++ assets/js/texts-krutidev.js
@@ -1,1 +1,12 @@
-Paste here
+/**
+ * KRUTI DEV 010 / DEVLYS TYPING PARAGRAPHS
+ * File: assets/js/texts-krutidev.js
+ */
+
+const KrutiDevTextDatabase = [
+    "f'k{kk lHkh ds fy, vko';d gSA f'k{kk ysus gsrq fdlh rjg dk 'kqYd ;k Qhl ugha gksuh pkfg,A",
+    
+    "Paste your next Kruti Dev text string here.",
+    
+    "dyke lkgc dks dkSu ugha tkurk gSA th dyke lkgc Hkkjr ds felkby eSuA lk/kkj.k gksrs gq, Hkh cgqr gh [kkl balkuA"
+];
