b0ada4c02cba5f05db3b201d986a612808b68e7c
manuhiteshsharma-lab <manuhiteshsharma@gmail.com>
2026-05-24 15:52:39 +0530

    Create texts-english.js

---
+++ assets/js/texts-english.js
@@ -0,0 +1,12 @@
+/**
+ * ENGLISH TYPING PARAGRAPHS
+ * File: assets/js/texts-english.js
+ */
+
+const EnglishTextDatabase = [
+    "Paste your first English paragraph here. Make sure it stays inside the quotation marks.",
+    
+    "This is your second English paragraph. The engine will randomly select one of these paragraphs every time a user starts a test.",
+    
+    "You can add as many paragraphs as you want. Just remember to put a comma at the end of every line except the very last one."
+];
